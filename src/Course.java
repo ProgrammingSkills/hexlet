a9b469c3d937aa9938ad2b91d9a16460967d5113
Viktor <ViktorsPersonalMail@gmail.com>
2013-07-25 01:51:04 +0300

    Empty files

--- src/Course.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Xibalba
- * Date: 11.07.13
- * Time: 21:14
- * To change this template use File | Settings | File Templates.
- */
-public class Course {
-}
